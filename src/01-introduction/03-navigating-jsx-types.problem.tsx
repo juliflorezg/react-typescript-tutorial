a11ba165e0e3844b2e937a0e19286f61dfa3328f
Julian Florez <forezjuli08@hotmail.com>
2023-05-17 18:52:57 -0500

    add code for exercise 03

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,11 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      aria-posinset={3}
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={e => {}}
 
-      // How do I get autocomplete with JSX?
+      // How do I get autocomplete with JSX? // using ctrl + space 
     />
-  );
-};
+  )
+}
