b88ea081ec74367dd192d3eb0d0c6fb08780d64c
Julian Florez <forezjuli08@hotmail.com>
2023-05-18 15:52:27 -0500

    add solution for exercise 15 - hooks

--- src/03-hooks/15-use-callback.problem.tsx
+++ src/03-hooks/15-use-callback.problem.tsx
@@ -1,21 +1,30 @@
-import { useCallback } from "react";
-import { Equal, Expect } from "../helpers/type-utils";
+import { useCallback } from 'react'
+import { Equal, Expect } from '../helpers/type-utils'
 
 export const Buttons = (props: { id: string }) => {
-  const onClick = useCallback<string>(
-    (buttonName) => {
-      console.log(props.id, buttonName);
+  //!! const onClick = useCallback<string>(
+  //> useCallback has to be of type Function
+  // const onClick = useCallback<(buttonName: string) => void>(
+  //   buttonName => {
+  //     console.log(props.id, buttonName)
+  //   },
+  //   [props.id]
+  // )
+  //> alternatively, we can rely on inference and pass the type to tye argument
+  const onClick = useCallback(
+    (buttonName: string) => {
+      console.log(props.id, buttonName)
     },
-    [props.id],
-  );
+    [props.id]
+  )
 
-  type test = Expect<Equal<typeof onClick, (buttonName: string) => void>>;
+  type test = Expect<Equal<typeof onClick, (buttonName: string) => void>>
 
   return (
     <div>
-      <button onClick={() => onClick("A")}>A</button>
-      <button onClick={() => onClick("B")}>B</button>
-      <button onClick={() => onClick("C")}>C</button>
+      <button onClick={() => onClick('A')}>A</button>
+      <button onClick={() => onClick('B')}>B</button>
+      <button onClick={() => onClick('C')}>C</button>
     </div>
-  );
-};
+  )
+}
