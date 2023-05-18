030995cac0902dfd30db5550857e382f352938ee
Julian Florez <forezjuli08@hotmail.com>
2023-05-18 10:24:09 -0500

    add solution for exercise 08

--- src/02-components/08-using-html-props.problem.tsx
+++ src/02-components/08-using-html-props.problem.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { ButtonHTMLAttributes, ComponentProps } from 'react'
 
-export const Button = ({ className, ...rest }: {}) => {
+// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+//   className?: string
+// }
+
+// export const Button = ({ className, ...rest }: ButtonProps) => {
+export const Button = ({ className, ...rest }: ComponentProps<'button'>) => {
   return (
     <button {...rest} className={`default-classname ${className}`}></button>
-  );
-};
+  )
+}
 
 const Parent = () => {
-  return <Button onClick={() => {}} type="button"></Button>;
-};
+  return <Button onClick={() => {}} type="button"></Button>
+}
